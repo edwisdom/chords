56fa0df163c934286c1819936e6da84d6db7a08d
Chris Phifer <cphifer97@gmail.com>
2020-07-23 23:51:42 -0400

    Added `Base.Quality`, which exports a canonicalization function

--- src/Base/Chord.hs
+++ src/Base/Chord.hs
@@ -2,15 +2,13 @@ module Base.Chord
   ( canonicalizeChord
   ) where
 
+import Base.Quality
+
 import Base.Core.Quality
 
 import Base.Chord.Chord
 import Base.Chord.HighestNatural
 import Base.Chord.RawChord
-
-canonicalizeQuality :: Maybe Quality -> HighestNatural -> Quality
-canonicalizeQuality Nothing  hn = if getDegree hn < 7 then QMajor else QDominant
-canonicalizeQuality (Just q) _  = q
 
 canonicalizeChord :: RawChord -> Chord
 canonicalizeChord rc =
---
+++ src/Base/Quality.hs
@@ -0,0 +1,11 @@
+module Base.Quality
+  ( canonicalizeQuality
+  ) where
+
+import Base.Core.Quality
+
+import Base.Chord.HighestNatural
+
+canonicalizeQuality :: Maybe Quality -> HighestNatural -> Quality
+canonicalizeQuality Nothing  hn = if getDegree hn < 7 then QMajor else QDominant
+canonicalizeQuality (Just q) _  = q
