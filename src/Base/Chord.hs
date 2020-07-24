197dee26c57d756549c3baf5b8656596cafbb70e
Chris Phifer <cphifer97@gmail.com>
2020-07-23 23:47:39 -0400

    Small lint / format change

--- src/Base/Chord.hs
+++ src/Base/Chord.hs
@@ -9,8 +9,8 @@ import Base.Chord.HighestNatural
 import Base.Chord.RawChord
 
 canonicalizeQuality :: Maybe Quality -> HighestNatural -> Quality
-canonicalizeQuality Nothing  hn = if (getDegree hn) < 7 then QMajor else QDominant
-canonicalizeQuality (Just q) _                    = q
+canonicalizeQuality Nothing  hn = if getDegree hn < 7 then QMajor else QDominant
+canonicalizeQuality (Just q) _  = q
 
 canonicalizeChord :: RawChord -> Chord
 canonicalizeChord rc =
