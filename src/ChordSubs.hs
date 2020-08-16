887db3e47d5d930a4c7ef84949a56c37992c3196
Edwisdom <ejain49@gmail.com>
2020-08-16 01:57:22 -0400

    ADD extend1 chord sub

--- src/ChordSubs.hs
+++ src/ChordSubs.hs
@@ -1,6 +1,6 @@
 module ChordSubs
   ( remove5
-  ,
+  , extend1
   ) where 
 
 
@@ -22,10 +22,22 @@ import Data.Set(Set(..), toList, delete)
 
 
 
-
-remove5 :: Chord -> [Root] -> (Chord, [Root])
-remove5 chord _ = (chord, (toNotes (getChordRoot chord) (delete (intervalFrom Perfect 5) (chordToIntervals chord))))
+remove5 :: (Chord, [Root]) -> (Chord, [Root])
+remove5 (chord, _) = (chord, (toNotes (getChordRoot chord) (delete (intervalFrom Perfect 5) (chordToIntervals chord))))
   where
     toNotes :: Root -> Set Interval -> [Root]
     toNotes root intSet = flip jumpIntervalFromNote root <$> toList intSet
 
+
+extend1 :: (Chord, [Root]) -> (Chord, [Root])
+extend1 (chord, _) =  (newChord, chordToNotes newChord)
+  where
+    newChord = chordFrom (getChordRoot chord) (C.getQuality chord)
+               (extendHighestNat (getHighestNatural chord))
+               (getExtensions chord) (getSus chord)
+    extendHighestNat :: HighestNatural -> HighestNatural
+    extendHighestNat highNat = 
+      if isMajor highNat then
+        majorNatural (getDegree highNat + 2)
+      else 
+        nonMajorNatural (getDegree highNat + 2)
