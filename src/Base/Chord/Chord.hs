6633b547fa1811f0c8d67c1b68b878f706324300
Chris Phifer <cphifer97@gmail.com>
2020-08-27 14:50:20 -0500

    Updated `Chord` type to use the new `ChordShape` and `ChordSymbol` types. The interface didn't change, though, so all other code using `Chord` should work as expected. Note that the list of notes in the chord are not currently added by the constructor.

--- src/Base/Chord/Chord.hs
+++ src/Base/Chord/Chord.hs
@@ -8,19 +8,39 @@ module Base.Chord.Chord
   , chordFrom
   ) where
 
-import Base.Core.Quality.CQuality
-
+import qualified Base.Chord.ChordShape as Shape
+import qualified Base.Chord.ChordSymbol as Sym
 import Base.Chord.Extension
 import Base.Chord.HighestNatural
 import Base.Chord.Root
 import Base.Chord.Sus
 
-data Chord = Chord { getChordRoot :: Root
-                   , getQuality :: Quality
-                   , getHighestNatural :: HighestNatural
-                   , getExtensions :: [Extension]
-                   , getSus :: Sus
+import Base.Core.Note
+import Base.Core.Quality.CQuality
+
+data Chord = Chord { getSymbol :: Sym.ChordSymbol
+                   , getNotes :: [Note]
                    } deriving Show
 
+getChordRoot :: Chord -> Root
+getChordRoot = Sym.getChordRoot . getSymbol
+
+getQuality :: Chord -> Quality
+getQuality = Shape.getQuality . Sym.getShape . getSymbol
+
+getHighestNatural :: Chord -> HighestNatural
+getHighestNatural = Shape.getHighestNatural . Sym.getShape . getSymbol
+
+getExtensions :: Chord -> [Extension]
+getExtensions = Shape.getExtensions . Sym.getShape . getSymbol
+
+getSus :: Chord -> Sus
+getSus = Shape.getSus . Sym.getShape . getSymbol
+
+-- TODO: Actually get `chordToNotes` somewhere where we can put the real list
+-- of notes here
 chordFrom :: Root -> Quality -> HighestNatural -> [Extension] -> Sus -> Chord
-chordFrom = Chord
+chordFrom r q hn exts s =
+  Chord { getSymbol = Sym.chordSymbolFrom r $ Shape.chordShapeFrom q hn exts s
+        , getNotes = []
+        }
