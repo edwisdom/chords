456af67879877d2ae1eba4436ef96955cbeff00d
Chris Phifer <cphifer97@gmail.com>
2020-08-27 14:10:46 -0500

    Implemented the `ChordSymbol` module

---
+++ src/Base/Chord/ChordSymbol.hs
@@ -0,0 +1,26 @@
+{-|
+Module      : Base.Chord.ChordSymbol
+Description : Abstract representation of a chord symbol
+Copyright   : (c) Uhhhh
+License     : GPL-3
+Maintainers : cphifer@galois.com, ejain49@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Abstractly represent a chord symbol, i.e. a chord shape with a specified root
+note.
+-}
+
+module Base.Chord.ChordSymbol
+  (
+  ) where
+
+import Base.Chord.ChordShape
+import Base.Chord.Root
+
+data ChordSymbol = ChordSymbol { getRoot :: Root
+                               , getShape :: ChordShape
+                               } deriving Show
+
+chordSymbolFrom :: Root -> ChordShape -> ChordSymbol
+chordSymbolFrom = ChordSymbol
