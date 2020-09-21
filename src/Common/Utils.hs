8b7aee0091ec1577e42461094d2d71e1b2fdb23b
Chris Phifer <cphifer97@gmail.com>
2020-09-20 22:14:29 -0500

    Added a newline to the end of the file

--- src/Common/Utils.hs
+++ src/Common/Utils.hs
@@ -21,7 +21,7 @@ module Common.Utils
   , isUnique
   ) where
 
-import Data.Set (fromList)
+import Data.Set (Set, fromList)
 
 -- | Given a modulus and the new lower bound, returns a function
 -- to mod a number accordingly.
